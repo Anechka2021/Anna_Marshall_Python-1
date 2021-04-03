7ec7a11e84872c88be10aad1699d4675c68a11ed
Anna <anna.marshall@outlook.com>
2021-04-03 22:11:38 +0100

    Lesson 1 task 1

---
+++ Lesson 1-1.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ")
+print("Hi!", name)
+
+age = input("Enter your age: ")
+print("You are so young,", name)
+
+or_pass= "1234"
+password = input ("Enter your password:")
+
+if password == or_pass:
+    print("Ok")
