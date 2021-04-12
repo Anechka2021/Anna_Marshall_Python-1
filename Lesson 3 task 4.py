1a7553d963f6584cb1e2a548da08ad2a8735a3f2
Anna Marshall <80423641+Anechka2021@users.noreply.github.com>
2021-04-12 23:22:28 +0100

    Lesson 3 task 4

---
+++ Lesson 3 task 4.py
@@ -0,0 +1,7 @@
+def my_pow_fun(x, y):
+    try:
+        res = x ** y
+    except TypeError:
+        return "Enter a number and a number in a negative power: "
+    return res
+print(my_pow_fun(3.4, -4))
