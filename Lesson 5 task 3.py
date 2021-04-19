10c2b7b641db663427604ab01b95fcbccfd79520
Anna Marshall <80423641+Anechka2021@users.noreply.github.com>
2021-04-19 23:39:06 +0100

    Lesson 5 task 3

---
+++ Lesson 5 task 3.py
@@ -0,0 +1,17 @@
+with open('sal.txt', 'r') as my_file:
+    sal = []
+    poor = []
+    my_list = my_file.read().split('\n')
+    for i in my_list:
+        i = i.split()
+        if int(i[1]) < 20000:
+           poor.append(i[0])
+        sal.append(i[1])
+print(f'Оклад меньше 20.000 {poor}, средний оклад {sum(map(int, sal)) / len(sal)}')
+
+#-------------------------------------------------------------------------------------------------#
+
+with open('text_3.txt', 'r', encoding='utf-8') as f:
+    employees_dict = {line.split()[0]: float(line.split()[1]) for line in f}
+    print(f'Average salary = {round(sum(employees_dict.values()) / len(employees_dict), 3)}\n'
+    f'Employees with salary of less than 20K {[e[0] for e in employees_dict.items() if e[1] < 20000]}')
