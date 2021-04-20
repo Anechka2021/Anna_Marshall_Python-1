93ee5756e22be5faadfd8fed94b988617ea372be
Anna Marshall <80423641+Anechka2021@users.noreply.github.com>
2021-04-20 17:20:43 +0100

    Lesson 5 task 7

--- Lesson 5 task 7.py
+++ Lesson 5 task 7.py
@@ -1,19 +1,29 @@
-if __name__ == "__main__":
-    subjects = {}
 
-    try:
-        with open(FILENAME, encoding='utf-8') as fh:
-            lines = fh.readlines()
+import json
+profit = {}
+pr = {}
+prof = 0
+prof_aver = 0
+i = 0
+with open('file_7.txt', 'r') as file:
+    for line in file:
+        name, firm, earning, damage = line.split()
+        profit[name] = int(earning) - int(damage)
+        if profit.setdefault(name) >= 0:
+            prof = prof + profit.setdefault(name)
+            i += 1
+    if i != 0:
+        prof_aver = prof / i
+        print(f'Прибыль средняя - {prof_aver:.2f}')
+    else:
+        print(f'Прибыль средняя - отсутсвует. Все работают в убыток')
+    pr = {'средняя прибыль': round(prof_aver)}
+    profit.update(pr)
+    print(f'Прибыль каждой компании - {profit}')
 
-        for line in lines:
-            data = line.replace('(', ' ').split()
+with open('file_7.json', 'w') as write_js:
+    json.dump(profit, write_js)
 
-            subjects[data[0][:-1]] = sum(
-                int(i) for i in data if i.isdigit()
-            )
-    except IOError as e:
-        print(e)
-    except ValueError:
-        print("Неконсистентные данные")
-
-    print(subjects)
+    js_str = json.dumps(profit)
+    print(f'Создан файл с расширением json со следующим содержимым: \n '
+          f' {js_str}')
